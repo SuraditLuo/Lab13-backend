40da05eab0e8051647ee812248e19b8987cdf880
Suradit Luo <suradit_luo@cmu.ac.th>
2022-10-19 17:42:26 +0700

    fix url and CORS

--- src/main/java/se331/rest/Lab08Application.java
+++ src/main/java/se331/rest/Lab08Application.java
@@ -19,7 +19,7 @@ public class Lab08Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://34.233.241.237:8001")
                         .exposedHeaders("x-total-count");
 
             }
